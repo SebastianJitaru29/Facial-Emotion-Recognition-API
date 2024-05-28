2db28835294f8f165b329b5bbe3b3307c8e1d377
SebastianJitaru <sjitaru29@gmail.com>
2024-05-28 22:35:06 +0200

    fix: firebase services calls

--- services/data/firebase_firestore.py
+++ services/data/firebase_firestore.py
@@ -6,10 +6,10 @@ class FirebaseService:
         self.firebase_cred_path = firebase_cred_path
         self.storage_client = None
     
-    def initialize_app(self):
-        cred = credentials.Certificate(self.firebase_cred_path)
-        initialize_app(cred, {'storageBucket': '<myBucket>'})
-        self.storage_client = storage.Client()
+    # def initialize_app(self):
+    #     cred = credentials.Certificate(self.firebase_cred_path)
+    #     initialize_app(cred, {'storageBucket': '<myBucket>'})
+    #     self.storage_client = storage.Client()
 
     def download_video_from_storage(self, video_name):
         # Implement video download logic here
