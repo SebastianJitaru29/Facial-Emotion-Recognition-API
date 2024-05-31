8bae3ac01523c4a42189dc7df1accec5c837733c
SebastianJitaru <sjitaru29@gmail.com>
2024-05-31 14:27:42 +0200

    initial: first branch commit

--- services/data/firebase_service.py
+++ services/data/firebase_service.py
@@ -1,6 +1,8 @@
 from firebase_admin import credentials, firestore, initialize_app
 from google.cloud import storage
 
+
+#Should be implemented using cloud functions to monitor storage state
 class FirebaseService:
     def __init__(self, firebase_cred_path):
         self.firebase_cred_path = firebase_cred_path
@@ -12,12 +14,10 @@ class FirebaseService:
     #     self.storage_client = storage.Client()
 
     def download_video_from_storage(self, video_name):
-        # Implement video download logic here
         print(video_name)
         return "static/videos/my_face_video.mp4"
         pass
 
     def upload_to_firestore(self, data):
-        # Implement Firestore upload logic here
         print(data)
         pass
