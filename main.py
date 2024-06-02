b53693ec7d1b6b7b4a1371fe22f59b331e08a4c8
SebastianJitaru <sjitaru29@gmail.com>
2024-06-02 16:21:57 +0200

    feat:added download from firebase storage feature and logging

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
+import logging
+import coloredlogs
 from services.emotion_analysis.emotion_analysis_imp import EmotionsAnalysisImp
 from services.data.firebase_imp import FirebaseImp
 
 def main():
-    # Initialize the FirebaseService
-    firebase_service = FirebaseImp(firebase_cred_path="firebase/backend-testing-tfg-firebase-adminsdk-vx45t-d0c10e29bd.json", storage_bucket="gs://backend-testing-tfg.appspot.com")
+    coloredlogs.install(level="INFO", fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    logger = logging.getLogger(__name__)
 
-    # Perform emotion analysis on the video
+    firebase_cred_path = "firebase/backend-testing-tfg-firebase-adminsdk-vx45t-d0c10e29bd.json"
+    storage_bucket = "backend-testing-tfg.appspot.com"  
+
+    logger.info(f"Initializing Firebase with credentials: {firebase_cred_path} and bucket: {storage_bucket}")
+    firebase_service = FirebaseImp(firebase_cred_path=firebase_cred_path, storage_bucket=storage_bucket)
+
+    # Perform emotion analysis on the video (currently commented out)
     emotion_analysis_service = EmotionsAnalysisImp(model_path="models/model2/model2.h5")
     video_path = "static/videos/my_face_video1.mp4"  # Path to the video you want to analyze
     result = emotion_analysis_service.get_emotion_percentages(video_path)
-    
-    print("The result is:", result)
-    
+    # print("The result is:", result)
     # Convert the result to a dictionary (if it isn't already)
     result_dict = result if isinstance(result, dict) else result.__dict__
-    print("The result in dictionary format is:", result_dict)
-    
-    # Download last video updated to firebase storage
-    #video_path = firebase_service.download_video_from_storage("my_face_video.mp4")
+    # print("The result in dictionary format is:", result_dict)
 
+    # Download last video updated to Firebase storage
+    video_name = "FB.mp4"
+    logger.info(f"Attempting to download video: {video_name} from storage.")
+    try:
+        video_path = firebase_service.download_video_from_storage(video_name)
+        logger.info(f"Video downloaded successfully to: {video_path}")
+    except Exception as e:
+        logger.error(f"Failed to download video: {e}")
+        print(f"Failed to download video: {e}")
 
-    # Upload the result to Firestore
+    # Upload the result to Firestore (currently commented out)
     doc_id = firebase_service.upload_to_firestore(result_dict)
     print("Document ID:", doc_id)
-
 
 if __name__ == "__main__":
     main()
--- services/data/firebase_imp.py
+++ services/data/firebase_imp.py
@@ -1,14 +1,22 @@
 import firebase_admin
+import logging
+import coloredlogs
 from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase_admin import storage
 from services.data.firebase_service import FirebaseService
 
 class FirebaseImp(FirebaseService):
+    
+    coloredlogs.install(level="INFO", fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    logger = logging.getLogger(__name__)
+
     def __init__(self, firebase_cred_path, storage_bucket):
         self.firebase_cred_path = firebase_cred_path
         self.storage_bucket = storage_bucket
         self._initialize_app()
+        coloredlogs.install(level="INFO", fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        self.logger = logging.getLogger(__name__)
         self.db = firestore.client()
 
     def _initialize_app(self):
@@ -18,8 +26,9 @@ class FirebaseImp(FirebaseService):
 
     def download_video_from_storage(self, video_name):
         blob = self.storage_client.blob(video_name)
-        video_path = f"{video_name}"
-        blob.download_to_filename(video_path)
+        video_path = f"static/videos/{video_name}"
+        blob.download_to_filename(f"{video_path}")
+        self.logger.info(f"Video downloaded successfully to: {video_path}")
         return video_path
 
     def upload_to_firestore(self, data):
