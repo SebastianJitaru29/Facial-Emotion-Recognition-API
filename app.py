ef86b636097aa56a46b081a4618f159debfee960
SebastianJitaru <sjitaru29@gmail.com>
2024-06-07 12:59:00 +0200

    feat: dockerize backend started

--- app.py
+++ app.py
@@ -2,12 +2,18 @@ from flask import Flask
 from routes.video_routes import video_routes
 import logging
 import coloredlogs
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+load_dotenv()
 app.register_blueprint(video_routes)
+
+app.config["DEBUG"] = os.environ.get("FLASK_DEBUG")
+
 
 if __name__ == "__main__":
     coloredlogs.install(level="INFO", fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     logger = logging.getLogger(__name__)
     logger.info("Starting the application on: http://localhost:5000/")
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000)
--- utils/record_face.py
+++ utils/record_face.py
@@ -1,33 +1,25 @@
 import cv2
 
 def record_video(filename, duration):
-    # Open the default camera
     cap = cv2.VideoCapture(0)
 
-    # Define the codec and create VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter(filename, fourcc, 20.0, (640, 480))
 
     start_time = cv2.getTickCount()
     while True:
-        # Capture frame-by-frame
         ret, frame = cap.read()
 
-        # Write the frame into the file
         out.write(frame)
 
-        # Display the resulting frame
         cv2.imshow('frame', frame)
 
-        # Break the loop if the duration exceeds
         if (cv2.getTickCount() - start_time) / cv2.getTickFrequency() > duration:
             break
 
-        # Break the loop if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    # Release everything when done
     cap.release()
     out.release()
     cv2.destroyAllWindows()
